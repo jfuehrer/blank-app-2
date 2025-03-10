ddc9f39ecacbaaf4bffe8b124f8eff63375b3fb9
jfuehrer <jfuehrer@mitre.org>
2025-03-10 11:48:28 +0000

    updated IC

---
+++ innovation_capacity.py
@@ -0,0 +1,5 @@
+# Innovation capacity (e.g., R&D investments as a percentage of revenue, 
+# history of patent filings or
+#  new product launches); 
+# starting with R&D investments as a % of revenue; see scoring framework research
+
