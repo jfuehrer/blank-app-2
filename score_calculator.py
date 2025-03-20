1d305148e68bdc52b4457b58d5f060e2ca3776d2
jfuehrer <jfuehrer@mitre.org>
2025-03-20 16:49:29 +0000

    risk score reversed

--- score_calculator.py
+++ score_calculator.py
@@ -24,11 +24,11 @@ def calculate_vendor_risk_reliability(financial, past, contract, labor):
 
 # contains the risk category thresholds and interpretations for VRRS and other scores
 VRRS_RISK_THRESHOLDS = [
-    (8.5, float('inf'), "Very Low Risk", "Vendor is financially stable, has strong past performance, diverse federal contracts, and none to low foreign labor dependency. Recommend for partnership."),
-    (7.0, 8.49, "Low Risk", "Vendor is mostly stable with minor past performance or financial concerns and none to low foreign labor dependency. Recommend for partnerships."),
+    (8.5, float('inf'), "Severe Risk", "Vendor is highly unreliable, with financial distress, contract cancellations, compliance violations, or high foreign labor dependency risks for critical roles. Not recommended for contracts."),
+    (7.0, 8.49, "High Risk", "Vendor shows significant financial weaknesses, federal contract issues, or foreign labor dependency risks for critical roles. Use only with mitigation strategies."),
     (5.0, 6.99, "Moderate Risk", "Vendor shows some financial instability or contract fulfillment issues and low foreign labor dependency. Due diligence is required before awarding contracts."),
-    (3.0, 4.99, "High Risk", "Vendor shows significant financial weaknesses, federal contract issues, or foreign labor dependency risks for critical roles. Use only with mitigation strategies."),
-    (1.0, 2.99, "Severe Risk", "Vendor is highly unreliable, with financial distress, contract cancellations, compliance violations, or high foreign labor dependency risks for critical roles. Not recommended for contracts.")
+    (3.0, 4.99, "Low Risk", "Vendor is mostly stable with minor past performance or financial concerns and none to low foreign labor dependency. Recommend for partnerships."),
+    (1.0, 2.99, "Very Low Risk", "Vendor is financially stable, has strong past performance, diverse federal contracts, and none to low foreign labor dependency. Recommend for partnership.")
 ]
 
 def determine_risk_category(score, thresholds=VRRS_RISK_THRESHOLDS):
